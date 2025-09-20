42cc64cf6dd708159d1d98caf3cdf8dc2a34366a
samaaledan <aledan@sheridancollege.ca>
2025-09-19 21:06:47 -0400

    Completed ICE1 CardTrick (with lucky card check)

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,52 +1,51 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package card;
+package Card;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author srinivsi
+ * Card model for ICE 1 
+ * Modifier: SamaAledan #991741787
+ * Date modified: 2025-09-17
  */
+
 public class Card {
-
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
-
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
-
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
+    // 1..13 (ACE..king)
+    private int value;
+    // One of SUITS
+    private String suit;
+    
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    
+    public Card(int value, String suit) {
+        this.value = value;
         this.suit = suit;
     }
-
-    /**
-     * @return the value
-     */
-    public int getValue() {
-        return value;
-    }
-
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
     
+    public int getValue() {return value;}
+    public void setValue(int value) {this.value = value;}
+    
+    public String getSuit() {return suit;}
+    public void setSuit(String suit) {this.suit = suit;}
+    
+    @Override
+    public String toString(){
+        return suit + " " + value;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Card)) return false;
+        Card c = (Card) o;
+        return value == c.value && suit.equals(c.suit);
+    }
+    
+    @Override
+    public int hashCode() {
+        return 31 * value + suit.hashCode();
+    }
 }
+
